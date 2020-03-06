ebff3e384d7d47dee797a62d63f29e6e027e8c8f
unknown <serchatchasan@gmail.com>
2020-03-06 19:08:23 +0200

    dorean

--- calculator/calculator/Calcula.h
+++ calculator/calculator/Calcula.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Calculator.h"
 
 namespace calculator {
 
@@ -14,6 +15,9 @@ namespace calculator {
 	/// </summary>
 	public ref class Calcula : public System::Windows::Forms::Form
 	{
+		int num1 = 0;
+		int num2 = 0;
+		bool isAdd = false;
 	public:
 		Calcula(void)
 		{
@@ -34,6 +38,11 @@ namespace calculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
 
 	private:
 		/// <summary>
@@ -48,12 +57,98 @@ namespace calculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Calcula";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->button1->Location = System::Drawing::Point(27, 117);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 32);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"=";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Calcula::button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->numericUpDown1->Location = System::Drawing::Point(0, 0);
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(428, 20);
+			this->numericUpDown1->TabIndex = 1;
+			this->numericUpDown1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(27, 75);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"+";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Calcula::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(108, 75);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"-";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &Calcula::button3_Click);
+			// 
+			// Calcula
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(428, 320);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->numericUpDown1);
+			this->Controls->Add(this->button1);
+			this->Name = L"Calcula";
+			this->Text = L"Calcula";
+			this->Load += gcnew System::EventHandler(this, &Calcula::Calcula_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			this->ResumeLayout(false);
+
 		}
+		
 #pragma endregion
-	};
+	private: System::Void Calcula_Load(System::Object^ sender, System::EventArgs^ e) {
+
+		numericUpDown1->Controls[0]->Visible = false;
+		
+
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		num1 = Convert::ToInt32(numericUpDown1->Text->ToString());
+		isAdd = true;
+		numericUpDown1->Text = "";
+	}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		num1 = Convert::ToInt32(numericUpDown1->Text->ToString());
+		isAdd = false;
+		numericUpDown1->Text = "";
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	Calculator calc = Calculator();
+	num2 = Convert::ToInt32(numericUpDown1->Text->ToString());
+	if (isAdd) {
+
+		numericUpDown1->Text = calc.add(num1, num2).ToString();
+	}
+	else {
+		calc.sub(num1, num2);
+		numericUpDown1->Text = calc.sub(num1, num2).ToString();
+	}
+}
+};
 }
